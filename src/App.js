725c5d92e354365082b16c68b4caf134614b08e4
RakibHosen0 <rakib52112@gmail.com>
2021-02-23 14:44:08 +0600

    map function

--- src/App.js
+++ src/App.js
@@ -2,21 +2,53 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const products = [
+    { name: "Photoshopa", price: "$90" },
+    { name: "PDF Reder", price: "$6.99" },
+    { name: "Html ", price: "$5.99" },
+    { name: "Web Development", price: "$6.99" },
+  ];
+  function Product(props) {
+    const productStyle = {
+      border: "1px solid gray",
+      borderRadius: "5px",
+      backgroundColor: "lightblue",
+      height: "200px",
+      width: "200px",
+      textAlign: "center",
+    };
+    const { name, price } = props.product;
+    return (
+      <dir style={productStyle}>
+        <h3>Name :{name} </h3>
+        <h2>{price}</h2>
+        <button>Buy Now</button>
+      </dir>
+    );
+  }
+
+  function Person(props) {
+    return (
+      <div
+        style={{ border: "2px solid yellow", padding: "10px", margin: "10px" }}
+      >
+        <h>{props.name}</h>
+        <h2>{props.job}</h2>
+      </div>
+    );
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Done <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {products.map((prod) => (
+          <Product product={prod}></Product>
+        ))}
+
+        {/* <Product product={products[0]}></Product>
+        <Product product={products[1]}></Product> */}
+        <p>I am a react person</p>
+        <Person name="Rakib Hosen" job="Software Engineer"></Person>
+        <Person name="Faysal Ahmed" job="Networing Engineer"></Person>
       </header>
     </div>
   );
